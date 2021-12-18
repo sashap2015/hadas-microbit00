72e20da671d168b7dbd7a6bb716ccf8e15d0caeb
sashap2015 <poleshuka@gmail.com>
2021-12-18 22:40:39 +0200

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-basic.show_string("Hello!")
-basic.pause(100)
+def onEvery_interval():
+    mypause = 45
+    basic.show_string("HA")
+    basic.pause(mypause)
+    basic.show_string("DA")
+    basic.pause(mypause)
+    basic.show_string("S")
+    pass
+loops.every_interval(500, onEvery_interval)
+
--- main.ts
+++ main.ts
@@ -1,2 +1,9 @@
-basic.showString("Hello!")
-basic.pause(100)
+loops.everyInterval(500, function onEvery_interval() {
+    let mypause = 45
+    basic.showString("HA")
+    basic.pause(mypause)
+    basic.showString("DA")
+    basic.pause(mypause)
+    basic.showString("S")
+    
+})
